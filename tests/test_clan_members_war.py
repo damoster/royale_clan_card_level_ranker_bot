9c9be2a9bd9820001dbb4440c990ebe2511b75f4
joseph.cheng <joseph.cheng@bp.com>
2022-01-17 08:54:26 +1100

    added test case but sill in progress

--- tests/test_clan_members_war.py
+++ tests/test_clan_members_war.py
@@ -2,15 +2,17 @@ import unittest
 from mockito import when
 
 from tests.resources import clash_royale_client_riverracelog
-from clan_members_war import ClanMembersWar, get_river_race_log
+from clan_members_war import ClanMembersWar, past_weeks_clan_war
 
 class TestRiverRaceLog(unittest.TestCase):
     def test_river_race(self):
         clan_members_war = ClanMembersWar()
-        when(clan_members_war.get_river_race_log).thenReturn(
+        clan_tag = "#9GULPJ9L"
+        # Mock API responses and certain function calls
+        when(clan_members_war.clash_royale_client).get_river_race_log(clan_tag).thenReturn(
             clash_royale_client_riverracelog.RIVER_RACE_LOG_API_RESPONSE
         )
-
+        clan_war_history = past_weeks_clan_war(clash_royale_client_riverracelog.RIVER_RACE_LOG_API_RESPONSE, clan_tag)
 if __name__ == '__main__':
     unittest.main()
     
